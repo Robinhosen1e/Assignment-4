63423a4ac2586a594cabf8cefcbb8d606981c98c
Robin Hosen <nikiaafri@gmail.com>
2026-02-27 15:09:10 +0600

    c

--- js/js.js
+++ js/js.js
@@ -1,19 +1,2 @@
-document.getElementById('Interview-btn').addEventListener('click', function(){
-     const InterviewBtn = document.getElementById('Interview-btn');
-
-    InterviewBtn.classList.appendChild('interviews')
-
-    const show = document.getElementById(id);
-    show.appendChild('interviews')
-})
 
 
-
-// function sowonly(){
-//     const InterviewBtn = document.getElementById('Interview-btn');
-
-//     InterviewBtn.classList.add('hidden')
-
-//     const show = document.getElementById(id);
-//     show.appendChild('interviews')
-// }
--- js/machin.js
+++ js/machin.js
@@ -1,8 +1,1 @@
-function sowonly(){
-    const InterviewBtn = document.getElementById('Interview-btn');
 
-    InterviewBtn.classList.add('hidden')
-
-    const show = document.getElementById(id);
-    show.appendChild('interviews')
-}
